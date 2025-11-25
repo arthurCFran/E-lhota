1f88d0f0639d9208acb06c675c354a7b82d306f6
Arthur Francisco <Arthur Francisco>
2025-11-25 11:38:34 -0300

    'import { prisma } from './db/prisma.js'' comentado, esta dando erro por conta da falta do arquivo library do prisma.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { prisma } from './db/prisma.js'
+//import { prisma } from './db/prisma.js'
 
 const PORT = process.env.PORT || 3000
 
@@ -11,23 +11,23 @@ app.use(express.urlencoded())
 //nossas rotas
 
 app.get('/e_lhota', (request, response) => {
-    response.send("Hello Pedrés")
+    response.sendFile( 'index.html', { root: './views/html/' } )
 } )
 
-app.get('/api/pedras', async (request, response)=> {
-    /*await prisma.product.createMany({
+/*app.get('/api/pedras', async (request, response)=> {
+    await prisma.product.createMany({
         data: [
             {name : 'produto teste 1',price: 10},
             {name : 'produto teste 2',price: 24},
             {name : 'produto teste 3',price: 300}
         ]
-    })*/
+    })
 
     const pedras = await prisma.product.findMany({
         orderBy: { id: 'desc' }
     })
     response.json(pedras)
-})
+}) */
 
 
 app.listen(PORT, () => console.log(`API on http://localhost:${PORT}`))
