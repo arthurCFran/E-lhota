4f7b6c2f18fb1205e3e5212879565109ebb368ea
Luiz Henrique da Fonseca <8458219@univali.br>
2025-11-28 20:40:59 -0300

    Alteração de product.js, troca de api para busca no banco de dados

--- views/assets/javascript/module/products/product.js
+++ views/assets/javascript/module/products/product.js
@@ -1,8 +1,13 @@
-const API_URL_PRODUCTS = "https://fakestoreapi.com/products"
+
+const API_URL_PRODUCTS = "http://localhost:3000/products"
 
 const get = async () => {
   try {
     const response = await fetch(API_URL_PRODUCTS)
+    if (!response.ok){
+      throw new error ('Falha ao carregar os dados')
+    }
+
     const data = await response.json()
     return data
   } catch (error) {
